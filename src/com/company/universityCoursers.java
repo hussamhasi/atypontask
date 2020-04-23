2f6fb4e4bb0bc530e50bb6e60fd391b3575ec9f5
Hussam Hasi <hussamhasi@gmail.com>
2020-04-23 20:31:30 +0300

    my first commit.

---
+++ src/com/company/universityCoursers.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class universityCoursers {
+    List students =new ArrayList();
+    String Coursename;
+    String instructorname;
+
+    universityCoursers(String coursename,String instructorname){
+        this.Coursename=coursename;
+        this.instructorname=instructorname;
+    }
+
+}
